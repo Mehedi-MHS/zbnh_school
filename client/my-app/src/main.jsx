a828eb469835c72702f34ab26c937f890786fea1
Mehedi-MHS <mehedishuvo685638@gmail.com>
2023-10-07 16:20:42 +0600

    added navbar

--- client/my-app/src/main.jsx
+++ client/my-app/src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import { CssBaseline } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <CssBaseline>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CssBaseline>
   </React.StrictMode>
 );
