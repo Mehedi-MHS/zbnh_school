2955d6c7cf7947fac7804a7f711cb83137164b18
Mehedi-MHS <mehedishuvo685638@gmail.com>
2023-10-25 15:35:01 +0600

    Helmet dashboard

--- client/my-app/src/components/dashboard/DashboardHome.jsx
+++ client/my-app/src/components/dashboard/DashboardHome.jsx
@@ -1,10 +1,46 @@
 import Link from "../custom/CustomLink";
 import Button from "@mui/material/Button";
-
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardActions from "@mui/material/CardActions";
+import EditIcon from "@mui/icons-material/Edit";
+import IconButton from "@mui/material/IconButton";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 export default function DashboardHome() {
   return (
     <>
-      <h1> Dashboard Home</h1>
+      <Box
+        sx={{
+          width: "100%",
+          minHeight: "100vh",
+          padding: { xs: "2rem 0.5 rem", sm: "3rem 1rem" },
+          background: "silver",
+        }}
+      >
+        <Typography variant="h2" component="h1">
+          Admin Panel
+        </Typography>
+        <Card
+          sx={{
+            backgroundImage:
+              "radial-gradient(circle farthest-corner at top right,cyan,white)",
+          }}
+        >
+          <CardHeader
+            title="Students"
+            subheader="Manage students here"
+            action={
+              <IconButton>
+                <EditIcon />
+              </IconButton>
+            }
+          />
+          <CardActions>
+            <Button variant="outlined">Edit Students</Button>
+          </CardActions>
+        </Card>
+      </Box>
       <Link to="/dashboard/editStudents">
         <Button>Edit Students</Button>
       </Link>
