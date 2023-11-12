dcf846533b4b6a4de0f4118c92514dcd3556c236
Mehedi-MHS <mehedishuvo685638@gmail.com>
2023-11-12 15:43:31 +0600

    box update

--- client/my-app/src/components/teachers/TeachersHome.jsx
+++ client/my-app/src/components/teachers/TeachersHome.jsx
@@ -1,16 +1,18 @@
 import TeachersBox from "./TeachersBox";
 import SEO from "../custom/SEO";
-
+import { Box } from "@mui/material";
 export default function Teachers() {
   return (
     <>
-      <SEO
-        title="Our Honourable Teachers"
-        description="List of all the honourable teachers of ZamiderHat Begum Nurunnahar High School"
-        name="ZamiderHat Begum Nurunnahar High School"
-        type="Article"
-      />
-      <TeachersBox />
+      <Box sx={{ width: "100%", minHeight: "100vh" }}>
+        <SEO
+          title="Our Honourable Teachers"
+          description="List of all the honourable teachers of ZamiderHat Begum Nurunnahar High School"
+          name="ZamiderHat Begum Nurunnahar High School"
+          type="Article"
+        />
+        <TeachersBox />
+      </Box>
     </>
   );
 }
